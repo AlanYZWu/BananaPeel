e093cc2046fd4af0b3e92a0dc4cc0e670a65acef
AlanYZWu <alanyzwu@gmail.com>
2025-05-21 22:13:15 -0400

    Add pinyin to translation

--- content.js
+++ content.js
@@ -1,3 +1,5 @@
+import { pinyin } from "pinyin";
+
 // Function to detect if text is Chinese
 function isChinese(text) {
   return /[\u4e00-\u9fa5]/.test(text);
@@ -5,14 +7,26 @@ function isChinese(text) {
 
 // Function to translate text using Google Translate API
 async function translateText(text) {
-  const isChineseText = isChinese(text);
-  const targetLang = isChineseText ? 'en' : 'zh-CN';
-  const sourceLang = isChineseText ? 'zh-CN' : 'en';
+  const sourceChinese = isChinese(text);
+  const targetLang = sourceChinese ? 'en' : 'zh-CN';
+  const sourceLang = sourceChinese ? 'zh-CN' : 'en';
 
   try {
     const response = await fetch(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=${sourceLang}&tl=${targetLang}&dt=t&q=${encodeURIComponent(text)}`);
     const data = await response.json();
-    return data[0][0][0];
+    const translatedText = data[0][0][0];
+
+    // If translating to Chinese, add pinyin to translation
+    if (!sourceChinese) {
+      const pinyinText = pinyin(translatedText, {
+        segement: true,
+        group: true
+      }).join(' ');
+      return `${translatedText}\n${pinyinText}`;
+    }
+
+    // Return English translation
+    return translatedText;
   } catch (error) {
     console.error('Translation error:', error);
     return 'Translation failed';
